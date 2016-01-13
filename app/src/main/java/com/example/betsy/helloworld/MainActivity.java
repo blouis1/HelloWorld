31db40aab485c3a35838b0dac50391af4d2e66a6
Betsy Louis <blouis1@my.westga.edu>
2016-01-13 12:52:17 -0500

    Count decreases when minus button is clicked.

--- app/src/main/java/com/example/betsy/helloworld/MainActivity.java
+++ app/src/main/java/com/example/betsy/helloworld/MainActivity.java
@@ -44,6 +44,9 @@ public class MainActivity extends AppCompatActivity {
         if (v == plusButton) {
             counter++;
         }
+        if (v == minusButton) {
+            counter--;
+        }
         countTextView.setText(Integer.toString(counter));
     }
 
